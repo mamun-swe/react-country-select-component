79d51b02111a9d2da2ff6c58bf6bef9906f4b8df
Mamun-swe <mamun.swe.277@gmail.com>
2022-03-18 17:39:38 +0600

    Botstrap added

--- example/src/App.js
+++ example/src/App.js
@@ -1,21 +1,36 @@
 import React from 'react'
+import './index.css'
+import { Container, Row, Col, Card } from 'react-bootstrap'
 
 import { ReactCountrySelectComponent } from 'react-country-select-component'
 
 const App = () => {
   return (
-    <>
-      <ReactCountrySelectComponent
-        name={'country'}
-        isClearable={true}
-        error={false}
-        label='Country'
-        placeholder={'Select country'}
-        borderRadius={6}
-        defaultvalue={null}
-        onChange={(event) => console.log(event)}
-      />
-    </>
+    <div className='app-container'>
+      <Container>
+        <Row>
+          <Col xl={6} className="mx-auto">
+            <Card className='shadow rounded border-0'>
+              <Card.Header className='text-center border-0 bg-white pt-4'>
+                <h4 className='fw-bolder'>React country select component</h4>
+              </Card.Header>
+              <Card.Body>
+                <ReactCountrySelectComponent
+                  name={'country'}
+                  isClearable={true}
+                  error={false}
+                  label='Country'
+                  placeholder={'Select country'}
+                  borderRadius={6}
+                  defaultvalue={null}
+                  onChange={(event) => console.log(event)}
+                />
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,8 @@
-import './index.css'
+
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'))
