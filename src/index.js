38f84d16e7de0327a48bae6413b76e03f695bf30
Mamun-swe <mamun.swe.277@gmail.com>
2022-03-18 14:34:34 +0600

    React version update

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export const ReactCountrySelectComponent = ({ text }) => {
-  return <div className={styles.test}>Example country: {text}</div>
+  return <div className={styles.test}>Example country 1: {text}</div>
 }
